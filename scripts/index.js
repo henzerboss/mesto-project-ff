7b539f2233fb01080d91ba1c5720be9c31b357d9
Evgeny Evsyukov <henzerboss@gmail.com>
2025-02-18 12:37:30 +0100

    первая итерация ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+function createCard(cardName, cardLink) { // создал функцию, которая будет создавать карточки, слушать кнопку и удалять карточки
+    const cardTemplate = document.querySelector('#card-template').content; // получил элемент с id #card-template
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // клонировал шаблон из верстки
+    cardElement.querySelector('.card__image').src = cardLink; // значению аргумента src присвоил значение cardLink
+    cardElement.querySelector('.card__title').textContent = cardName; // значению аргумента src присвоил значение cardName
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) { // слушаю кнопку удаления
+        cardElement.remove(); // адьóс карточка!
+    });
+    
+    return cardElement; // возвращаю собранную карточку
+}
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list'); // получил элемент с классом .places__list
 
-// @todo: Функция создания карточки
+for (i = 0; i < initialCards.length; i++) { // перебираю массив с карточками
+    const cardName = initialCards[i].name; // выдергиваю из объекта название карточки
+    const cardLink = initialCards[i].link; // выдергиваю из объекта ссылку карточки
+    placesList.append(createCard(cardName, cardLink)); //добавляю все это на страницу
+}
 
-// @todo: Функция удаления карточки
+// The End
 
-// @todo: Вывести карточки на страницу
+console.log(`
+    /\\     /\\  
+   {  '---'  } 
+   {  O   O  } 
+   ~~>  V  <~~  
+    \\  ~~~  /   
+     '-----'  
+     /     \\  
+    {       }  
+     ~~~~~~~   
+   `);
