70c6d461c31e5f0e4990f05a443f7d412e0b466a
Evgeny Evsyukov <henzerboss@gmail.com>
2025-02-18 12:59:07 +0100

    Вынес удалялку карточек в отдельную функцию

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/henzerboss/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,30 @@
-function createCard(cardName, cardLink) { // создал функцию, которая будет создавать карточки, слушать кнопку и удалять карточки
-    const cardTemplate = document.querySelector('#card-template').content; // получил элемент с id #card-template
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // клонировал шаблон из верстки
-    cardElement.querySelector('.card__image').src = cardLink; // значению аргумента src присвоил значение cardLink
-    cardElement.querySelector('.card__title').textContent = cardName; // значению аргумента src присвоил значение cardName
-    cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) { // слушаю кнопку удаления
-        cardElement.remove(); // адьóс карточка!
-    });
-    
-    return cardElement; // возвращаю собранную карточку
+function createCard(cardName, cardLink) {
+  // создаю функцию, которая будет создавать карточки, слушать кнопку и удалять карточки
+  const cardTemplate = document.querySelector("#card-template").content; // получаю элемент с id #card-template
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); // клонирую шаблон из верстки
+  cardElement.querySelector(".card__image").src = cardLink; // значению аргумента src присваиваю значение cardLink
+  cardElement.querySelector(".card__title").textContent = cardName; // значению аргумента src присваиваю значение cardName
+  deleteCard(cardElement); //вызываю функцию удаления карточки
+  return cardElement; // возвращаю собранную карточку
 }
 
-const placesList = document.querySelector('.places__list'); // получил элемент с классом .places__list
+function deleteCard(cardElement) {
+  //создаю функцию удаления карточки
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function () {
+      // слушаю кнопку удаления
+      cardElement.remove(); // адьóс карточка!
+    });
+}
 
-for (i = 0; i < initialCards.length; i++) { // перебираю массив с карточками
-    const cardName = initialCards[i].name; // выдергиваю из объекта название карточки
-    const cardLink = initialCards[i].link; // выдергиваю из объекта ссылку карточки
-    placesList.append(createCard(cardName, cardLink)); //добавляю все это на страницу
+const placesList = document.querySelector(".places__list"); // получаю элемент с классом .places__list
+
+for (i = 0; i < initialCards.length; i++) {
+  // перебираю массив с карточками
+  const cardName = initialCards[i].name; // выдергиваю из объекта название карточки
+  const cardLink = initialCards[i].link; // выдергиваю из объекта ссылку карточки
+  placesList.append(createCard(cardName, cardLink)); //добавляю все это на страницу
 }
 
 // The End
