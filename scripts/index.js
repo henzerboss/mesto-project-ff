d59e7c47e01ebc608e87f64c1813579ee919ac7d
Evgeny Evsyukov <henzerboss@gmail.com>
2025-02-18 14:17:31 +0100

    Исправление замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,25 @@
-function createCard(cardName, cardLink) {
-  // создаю функцию, которая будет создавать карточки, слушать кнопку и удалять карточки
-  const cardTemplate = document.querySelector("#card-template").content; // получаю элемент с id #card-template
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); // клонирую шаблон из верстки
-  cardElement.querySelector(".card__image").src = cardLink; // значению аргумента src присваиваю значение cardLink
-  cardElement.querySelector(".card__title").textContent = cardName; // значению аргумента src присваиваю значение cardName
-  deleteCard(cardElement); //вызываю функцию удаления карточки
-  return cardElement; // возвращаю собранную карточку
+const placesList = document.querySelector(".places__list");
+
+function createCard(cardName, cardLink, cardAlt, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardLink;
+  cardElement.querySelector(".card__image").alt = cardAlt;
+  cardElement.querySelector(".card__title").textContent = cardName;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => deleteCard(cardElement));
+  return cardElement;
 }
 
 function deleteCard(cardElement) {
-  //создаю функцию удаления карточки
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", function () {
-      // слушаю кнопку удаления
-      cardElement.remove(); // адьóс карточка!
-    });
+  cardElement.remove();
 }
 
-const placesList = document.querySelector(".places__list"); // получаю элемент с классом .places__list
+initialCards.forEach((card) => {
+  const cardName = card.name;
+  const cardLink = card.link;
+  const cardAlt = `Изображение места: ${cardName}`;
+  placesList.append(createCard(cardName, cardLink, cardAlt, deleteCard));
+});
 
-for (i = 0; i < initialCards.length; i++) {
-  // перебираю массив с карточками
-  const cardName = initialCards[i].name; // выдергиваю из объекта название карточки
-  const cardLink = initialCards[i].link; // выдергиваю из объекта ссылку карточки
-  placesList.append(createCard(cardName, cardLink)); //добавляю все это на страницу
-}
-
-// The End
-
-console.log(`
-    /\\     /\\  
-   {  '---'  } 
-   {  O   O  } 
-   ~~>  V  <~~  
-    \\  ~~~  /   
-     '-----'  
-     /     \\  
-    {       }  
-     ~~~~~~~   
-   `);
